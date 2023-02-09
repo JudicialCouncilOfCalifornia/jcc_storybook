b983c7fe23912fc87da88bbc9f54066b35b27b5b
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2023-02-09 10:04:07 -0800

    First fields to get started.

--- src/stories/Atoms/FormFields/FormFields.data.js
+++ src/stories/Atoms/FormFields/FormFields.data.js
@@ -4,9 +4,7 @@ import drupalAttribute from 'drupal-attribute'
 export default {
   default: {
     variant: "default",
-    example: "FormFields component goes here.",
-    bg_tint: "accent-warm-dark-xx",
-    background_image_url: "https://source.unsplash.com/random/900×700/?building",
+    example: "Form Fields component goes here.",
     sub_component_data: {
       variant: "default",
       attributes: new drupalAttribute(),
@@ -20,6 +18,5 @@ export default {
       attributes: new drupalAttribute(),
     },
     attributes: new drupalAttribute()
-  },
-
+  }
 }
