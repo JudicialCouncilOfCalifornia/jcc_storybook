76ea46eec25c18da26465e17bb279ff229c97234
Shahnawaz Mohammed <shahnawaz.mohammed-t@jud.ca.gov>
2023-02-20 10:21:20 -0800

    fixed the email-signup as per the ticket

---
+++ src/stories/Molecules/EmailSingup/EmailSingup.js
@@ -0,0 +1,10 @@
+/**
+ * EmailSingup behavior.
+ *
+ * DELETE THIS FILE IF IT'S NOT NEEDED
+ * REMEMBER TO ALSO REMOVE THE import FROM EmailSingup.stories.js
+ *
+ * Remember jQuery is deprecated. Go vanilla!
+ * This will be wrapped in Drupal.behaviors on build, so no need for that here.
+ * Just write plain old Javascript.
+ **/
