d5392d485fd100270c152e47c72cf2699d6ebfdf
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2023-03-21 03:13:37 -0700

    fix: Refactor Pages after Section and Card changes.

--- src/stories/Pages/PreviewPage/PreviewPage.data.js
+++ src/stories/Pages/PreviewPage/PreviewPage.data.js
@@ -1,5 +1,7 @@
 import drupalAttribute from 'drupal-attribute'
 import logo from '../../Atoms/SiteBranding/Council-Logo.svg'
+import Card from '../../Molecules/Card/Card.twig'
+import TeaserPlusList from '../../Organisms/TeaserPlusList/TeaserPlusList.twig'
 
 export default {
   default: {
@@ -275,7 +277,23 @@ export default {
       attributes: new drupalAttribute()
     },
     section_1: {
+      first_component: false,
       variant: 'primary-dark-xx-stripe',
+      card_variant: 'card--media-right',
+      sub_component_layout: 'switcher',
+      // center, stack
+      align_items: false,
+      // center, switcher
+      measure: 69,
+      // cluster, grid, reel, stack, switcher
+      gap: false,
+      // grid
+      grid_min_width: false,
+      // with-sidebar
+      sidebar_direction: false,
+      // with-sidebar
+      sidebar_width: false,
+      background_image_url: "",
       brow_data: {
         part_one: "Introduction",
         part_two: false,
@@ -286,128 +304,128 @@ export default {
       text: false,
       first_component: false,
       button_data: false,
-      sub_component_template: "Card",
-      sub_component_data: {
-        variant: "media-right",
-        media: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UpnXQalWR-U?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-        media_caption: '<div class="caption"><p>Video text. Could be video title and video desription. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod.</p></div>',
-        heading: "About Language Access",
-        text: "<p>Describes Language Access at the courts as both physical and *human* translation resources available. Lorem ipsum dolor sit amet consectetur adipiscin lorem ispum bodlre</p><a href='#' class='button button--text'>See Language Resources</a><a href='#' class='button button--text'>Learn About Court Interpreters</a>",
-        button_data: false,
-        attributes: new drupalAttribute(),
-      },
+      sub_component_data: [
+        Card({
+          variant: "media-right",
+          media: '<iframe width="560" height="315" src="https://www.youtube.com/embed/UpnXQalWR-U?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
+          heading: "About Language Access",
+          text: "<p>Describes Language Access at the courts as both physical and *human* translation resources available. Lorem ipsum dolor sit amet consectetur adipiscin lorem ispum bodlre.</p><a href='#' class='button button--text'>See Language Resources</a><a href='#' class='button button--text'>Learn About Court Interpreters</a>",
+          attributes: new drupalAttribute(),
+        }),
+      ],
       attributes: new drupalAttribute(),
     },
     section_2: {
-      variant: "default",
+      first_component: false,
+      variant: 'default',
+      card_variant: '',
+      sub_component_layout: 'grid',
+      // center, stack
+      align_items: false,
+      // center, switcher
+      measure: false,
+      // cluster, grid, reel, stack, switcher
+      gap: false,
+      // grid
+      grid_min_width: '41',
+      // with-sidebar
+      sidebar_direction: false,
+      // with-sidebar
+      sidebar_width: false,
+      background_image_url: "",
+      brow_data: false,
       heading: 'Start Here For Language Access',
-      sub_component_template: "Cards",
-      sub_component_data: {
-        attributes: new drupalAttribute(),
-        variant: "",
-        card_width: "41ch",
-        items: [
-          {
-            variant: "striped",
-            icon_data: {
-              icon: "documents",
-              color: false,
-              attributes: new drupalAttribute(),
-            },
-            heading: "Language Access Resources for Courts",
-            text: false,
-            button_data: {
-              label: "Button",
-              href: "#",
-              variant: "primary",
-            },
+      text: false,
+      button_data: false,
+      sub_component_data: [
+        Card({
+          variant: "striped",
+          icon_data: {
+            icon: "documents",
+            color: false,
             attributes: new drupalAttribute(),
           },
-          {
-            icon_data: {
-              icon: "speaker",
-              color: false,
-              attributes: new drupalAttribute(),
-            },
-            heading: "Search for an Interpreter",
-            text: false,
-            button_data: {
-              label: "Button",
-              href: "#",
-              variant: "primary",
-            },
-            variant: "striped",
+          heading: "Language Access Resources for Courts",
+          text: false,
+          button_data: {
+            label: "Button",
+            href: "#",
+            variant: "primary",
+          },
+          attributes: new drupalAttribute(),
+        }),
+        Card({
+          icon_data: {
+            icon: "speaker",
+            color: false,
             attributes: new drupalAttribute(),
           },
-          {
-            icon_data: {
-              icon: "speaker",
-              color: false,
-              attributes: new drupalAttribute(),
-            },
-            heading: "Become an Court Interpreter",
-            text: false,
-            button_data: {
-              label: "Button",
-              href: "#",
-              variant: "primary",
-            },
-            variant: "striped",
+          heading: "Search for an Interpreter",
+          text: false,
+          button_data: {
+            label: "Button",
+            href: "#",
+            variant: "primary",
+          },
+          variant: "striped",
+          attributes: new drupalAttribute(),
+        }),
+        Card({
+          icon_data: {
+            icon: "speaker",
+            color: false,
             attributes: new drupalAttribute(),
           },
-          {
-            icon_data: {
-              icon: "exam",
-              color: false,
-              attributes: new drupalAttribute(),
-            },
-            heading: "Find Court Interpreter Exam Information",
-            text: false,
-            button_data: {
-              label: "Button",
-              href: "#",
-              variant: "primary",
-            },
-            variant: "striped",
+          heading: "Become an Court Interpreter",
+          text: false,
+          button_data: {
+            label: "Button",
+            href: "#",
+            variant: "primary",
+          },
+          variant: "striped",
+          attributes: new drupalAttribute(),
+        }),
+        Card({
+          icon_data: {
+            icon: "exam",
+            color: false,
             attributes: new drupalAttribute(),
           },
-        ],
-      },
-      attributes: new drupalAttribute(),
+          heading: "Find Court Interpreter Exam Information",
+          text: false,
+          button_data: {
+            label: "Button",
+            href: "#",
+            variant: "primary",
+          },
+          variant: "striped",
+          attributes: new drupalAttribute(),
+        }),
+      ],
+      attributes: new drupalAttribute().addClass('cards'),
     },
     section_3: {
-      brow: false,
+      first_component: false,
+      variant: 'warm-dark-x',
+      card_variant: '',
+      sub_component_layout: 'grid',
+      // center, stack
+      align_items: false,
+      // center, switcher
+      measure: false,
+      // cluster, grid, reel, stack, switcher
+      gap: false,
+      // grid
+      grid_min_width: '41',
+      // with-sidebar
+      sidebar_direction: false,
+      // with-sidebar
+      sidebar_width: false,
+      background_image_url: "",
+      brow_data: false,
       heading: 'Latest News',
       text: false,
-      sub_component_template: "TeaserPlusList",
-      sub_component_data: {
-        teaser: {
-          brow_data: {
-            part_one: 'Orders',
-            part_two: 'July 12, 2022',
-            attributes: new drupalAttribute(),
-          },
-          heading: "2DCA Protocols for In-Person Oral Argument",
-          text: "The Second Appellate District's Division Two will hear oral arguments in the courtroom on May 19, 2022. Access to the courtroom will be limited to the..."
-        },
-        list_items: [
-          {
-            link: {
-              label: "Job Announcement – Appellate Court Attorney",
-              href: "#"
-            },
-            footer: "May 25, 2022"
-          },
-          {
-            link: {
-              label: "Second District Court of Appeal to resume in‑person arguments effective May 2022",
-              href: "#"
-            },
-            footer: "May 25, 2022"
-          }
-        ],
-        attributes: new drupalAttribute(),
-      },
       button_data: {
         label: "See All News",
         href: "#",
@@ -415,56 +433,93 @@ export default {
         size: 'normal',
         attributes: new drupalAttribute(),
       },
-      variant: 'warm-dark-x',
-      first_component: false,
+      sub_component_data: [
+        TeaserPlusList({
+          teaser: {
+            brow_data: {
+              part_one: 'Orders',
+              part_two: 'July 12, 2022',
+              attributes: new drupalAttribute(),
+            },
+            heading: "2DCA Protocols for In-Person Oral Argument",
+            text: "The Second Appellate District's Division Two will hear oral arguments in the courtroom on May 19, 2022. Access to the courtroom will be limited to the..."
+          },
+          list_items: [
+            {
+              link: {
+                label: "Job Announcement – Appellate Court Attorney",
+                href: "#"
+              },
+              footer: "May 25, 2022"
+            },
+            {
+              link: {
+                label: "Second District Court of Appeal to resume in‑person arguments effective May 2022",
+                href: "#"
+              },
+              footer: "May 25, 2022"
+            }
+          ],
+          attributes: new drupalAttribute(),
+        }),
+      ],
       attributes: new drupalAttribute(),
     },
     section_4: {
+      first_component: false,
+      variant: 'white',
+      card_variant: '',
+      sub_component_layout: 'grid',
+      // center, stack
+      align_items: false,
+      // center, switcher
+      measure: false,
+      // cluster, grid, reel, stack, switcher
+      gap: false,
+      // grid
+      grid_min_width: '41',
+      // with-sidebar
+      sidebar_direction: false,
+      // with-sidebar
+      sidebar_width: false,
+      background_image_url: "",
       brow_data: false,
       heading: false,
       text: false,
-      sub_component_template: "Cards",
-      sub_component_data: {
-        variant: "",
-        cardWidth: "30ch",
-        attributes: new drupalAttribute(),
-        items: [
-          {
-            media: '<img src="https://source.unsplash.com/random/900×700/?office" />',
-            heading: "Interested in becoming a court interpreter?",
-            text: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu.</p>",
-            button_data: {
-              label: "Become a Court Interpreter",
-              href: "#",
-              variant: "primary",
-              size: "small",
-            },
-            // A variant type that contols the layout and style of the component.
-            variant: "bordered-center",
-            // drupalAttribute allows us to mock drupal attributes in storybook.
-            attributes: new drupalAttribute(),
-          },
-          {
-            media: '<div class="frame"><img src="https://source.unsplash.com/random/900×700/?building,old" /></div>',
-            heading: "LEP Court Users",
-            text: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu. Lorem ipsum dolor sit amet consectetur adipiscing dolor sit amet.</p>",
-            button_data: {
-              label: "Self-Help for LEP Court Users",
-              href: "#",
-              variant: "primary",
-              size: "small",
-            },
-            // A variant type that contols the layout and style of the component.
-            variant: "bordered-center",
-            // drupalAttribute allows us to mock drupal attributes in storybook.
-            attributes: new drupalAttribute(),
-          },
-        ],
-      },
       button_data: false,
-      variant: 'white',
-      first_component: false,
-      attributes: new drupalAttribute(),
+      sub_component_data: [
+        Card({
+          media: '<img src="https://source.unsplash.com/random/900×700/?office" />',
+          heading: "Interested in becoming a court interpreter?",
+          text: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu.</p>",
+          button_data: {
+            label: "Become a Court Interpreter",
+            href: "#",
+            variant: "primary",
+            size: "small",
+          },
+          // A variant type that contols the layout and style of the component.
+          variant: "bordered-center",
+          // drupalAttribute allows us to mock drupal attributes in storybook.
+          attributes: new drupalAttribute(),
+        }),
+        Card({
+          media: '<div class="frame"><img src="https://source.unsplash.com/random/900×700/?building,old" /></div>',
+          heading: "LEP Court Users",
+          text: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu. Lorem ipsum dolor sit amet consectetur adipiscing dolor sit amet.</p>",
+          button_data: {
+            label: "Self-Help for LEP Court Users",
+            href: "#",
+            variant: "primary",
+            size: "small",
+          },
+          // A variant type that contols the layout and style of the component.
+          variant: "bordered-center",
+          // drupalAttribute allows us to mock drupal attributes in storybook.
+          attributes: new drupalAttribute(),
+        }),
+      ],
+      attributes: new drupalAttribute().addClass('cards'),
     },
     footer: {
       site_branding_data: {
