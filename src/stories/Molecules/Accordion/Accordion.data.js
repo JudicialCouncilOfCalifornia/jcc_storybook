2a9413f1ecae242366b9eac6345392d4971fe0f6
Philip Boden <philip.boden@gmail.com>
2023-03-30 17:09:18 -0700

    [LWR-64] -- Added Accordion component.

---
+++ src/stories/Molecules/Accordion/Accordion.data.js
@@ -0,0 +1,26 @@
+// To mock drupal attributes in storybook.
+import drupalAttribute from 'drupal-attribute'
+
+export default {
+  default: {
+    variant: "default",
+    items: [
+      {
+        label: "What is the Judicial Council of California's role in the current court interpreter program?",
+        content: "<p>This is text in a paragraph. This content can realistically be any type of content. Cards, Sections, Media...any rendered content could be passed in.</p> <p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu.</p>",
+        attributes: new drupalAttribute(),
+      },
+      {
+        label: "Which Judicial Council panel oversees the court interpreters program?",
+        content: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu.</p>",
+        attributes: new drupalAttribute(),
+      },
+      {
+        label: "How can courts identify certified or registered court interpreters?",
+        content: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare.</p>",
+        attributes: new drupalAttribute(),
+      },
+    ],
+    attributes: new drupalAttribute()
+  }
+}
---
+++ src/stories/Molecules/Accordion/readme.md
@@ -0,0 +1,36 @@
+<!-- This is the general documentation layout. Add or remove any sections as needed, but try to stay consistent across components. -->
+
+# Accordion
+
+An Accordion component is made from a label and a content element. The content is displayed/hidden when the label summary is clicked.
+
+<details>
+  <summary>Inherited CSS Variables:</summary>
+  - `--name`: variant_name
+</details>
+
+<details>
+  <summary>Twig Variables:</summary>
+  ```
+  variant: "default",
+    items: [
+      {
+        label: "Text String",
+        content: "rendered content",
+        attributes: new drupalAttribute(),
+      },
+      {
+        label: "Text String",
+        content: "rendered content",
+        attributes: new drupalAttribute(),
+      },
+      {
+        label: "Text String",
+        content: "rendered content",
+        attributes: new drupalAttribute(),
+      },
+      ...
+    ],
+    attributes: new drupalAttribute()
+  ```
+</details>
