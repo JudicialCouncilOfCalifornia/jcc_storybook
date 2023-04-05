2eae548c8c5b3314db3a43bbdcab69576491761f
Philip Boden <philip.boden@gmail.com>
2023-04-05 15:40:13 -0700

    [LWR-64] -- Improved animations of accordions, and also implement animation reduction checks for accessibility.

--- src/stories/Molecules/Accordion/Accordion.js
+++ src/stories/Molecules/Accordion/Accordion.js
@@ -9,12 +9,18 @@ function docReady(fn) {
 }
 
 docReady(function () {
+
+  // If reduced animation motion is preferred, set the speed to 0 (instant).
+  const mediaQuery = window.matchMedia('(prefers-reduced-motion: reduce)');
+  const animation_speed = mediaQuery.matches ? 0 : 200;
+
   const summaries = Array.from(document.querySelectorAll('details summary'));
   summaries.forEach(summary => {
     // We only want to attach the click listener only to each details summary,
     // thats why we grab the summaries and then step back up to get the parent
     // detail. This way the click is not triggered by any content inside the
     // detail.
+
     if (!summary.classList.contains('js-accordion')) {
       summary.classList.add('js-accordion');
     
@@ -29,14 +35,27 @@ docReady(function () {
           setTimeout(() => {
             summary.closest('details').removeAttribute('open');
             summary.closest('details').classList.remove('closing');
-          }, 200)
+          }, animation_speed)
         } else {
           // If closed when clicked, open the detail. The delay helps improve the
           // fade in animation of the content.
           setTimeout(() => {
             summary.closest('details').setAttribute('open', '');
-          }, 200)
+          }, animation_speed)
         }
+
+        // Close any open details who werent clicked.
+        const details = Array.from(document.querySelectorAll('details'));
+        details.forEach(detail => {
+          if (detail.hasAttribute('open')) {
+            detail.classList.add('closing');
+            setTimeout(() => {
+              detail.removeAttribute('open');
+              detail.classList.remove('closing');
+            }, animation_speed)
+          }
+        });
+
       });
     }
   });
