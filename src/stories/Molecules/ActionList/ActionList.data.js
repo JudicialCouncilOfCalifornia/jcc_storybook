9a128f787f4be906307e5e3619a6456551b48f6c
baptisten <baptiste.niviere@gmail.com>
2023-04-19 12:29:59 -0700

    action lists

---
+++ src/stories/Molecules/ActionList/ActionList.data.js
@@ -0,0 +1,35 @@
+import drupalAttribute from 'drupal-attribute'
+
+export default {
+  default: {
+    variant: "default",
+    heading: "Getting Started",
+    text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque vitae tincidunt lectus, ut sodales augue. Pellentesque pellentesque ut nunc id varius. Maecenas maximus, tortor eu molestie congue, arcu odio tristique nulla, venenatis maximus ligula tellus non massa. Proin quis tortor cursus, lacinia ligula id, maximus tortor.",
+    links: [
+      {
+        label: "Certified Interpreter",
+        button_data :{
+          label: "Learn more",
+          href: "#",
+          variant: "secondary",
+        }
+      },
+      {
+        label: "Registered Interpreter",
+        button_data :{
+          label: "Learn more",
+          href: "#",
+          variant: "secondary",
+        }
+      },
+      {
+        label: "Certified interpreter for American SIgn Language (ASL)",
+        button_data :{
+          label: "Learn more",
+          href: "#",
+          variant: "secondary",
+        }
+      },
+    ],
+  }
+}
---
+++ src/stories/Molecules/ActionList/ActionList.stories.js
@@ -0,0 +1,29 @@
+import readme from './readme.md';
+import data from './ActionList.data';
+import ActionList from './ActionList.twig';
+import './ActionList.css';
+
+export default {
+  title: 'Molecules/ActionList',
+  component: ActionList,
+  parameters: {
+    notes: readme,
+  },
+  argTypes: {
+    attributes: {table: {disable: true}},
+    variant: {
+      description: "ActionList variant.",
+      table: {defaultValue: {summary: 'default'}},
+      control: { type: 'radio' },
+      options: ['default'],
+    },
+  },
+};
+
+const Template = ({ ...args }) => {
+  return ActionList({ ...args });
+};
+
+export const Default = Template.bind({});
+Default.args = data.default;
+
