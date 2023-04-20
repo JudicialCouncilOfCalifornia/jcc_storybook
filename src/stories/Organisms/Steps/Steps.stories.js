766b2e07681f50f6711e64a8e77bb5fa579c6fa1
Philip Boden <philip.boden@gmail.com>
2023-04-19 22:07:12 -0700

    [LWR-119] -- Steps component added

---
+++ src/stories/Organisms/Steps/Steps.stories.js
@@ -0,0 +1,34 @@
+import readme from './readme.md';
+import data from './Steps.data';
+import Steps from './Steps.twig';
+import './Steps.css';
+
+export default {
+  title: 'Organisms/Steps',
+  component: Steps,
+  parameters: {
+    notes: readme,
+    layout: "padded",
+  },
+  argTypes: {
+    attributes: {table: {disable: true}},
+    variant: {table: {disable: true}},
+    color: {
+      name: "Color",
+      description: "Background color of step numbers.",
+      table: {defaultValue: {summary: 'default'}},
+      control: { type: 'radio' },
+      options: ['default', 'warm', 'dark', 'primary', 'secondary'],
+    },
+  },
+};
+
+// Create Template for variant templates to bind to.
+const Template = ({ ...args }) => {
+  return Steps({ ...args });
+};
+
+// Bind the Default component variant for this component.
+export const Default = Template.bind({});
+// Data comes from the Steps.data.js file.
+Default.args = data.default;
