a3fa56d5276f74a3f5cf59d982e21d58a9bbf85c
Philip Boden <philip.boden@gmail.com>
2023-04-30 22:10:14 -0700

    [Feature] -- Addition of Body component

---
+++ src/stories/Molecules/Body/Body.data.js
@@ -0,0 +1,10 @@
+export default {
+  default: {
+    variant: "default",
+    heading: "Body component goes here.",
+    lead: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quod quidem iam fit etiam in Academia.</p>",
+    content: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quod quidem iam fit etiam in Academia. Prave, nequiter, turpiter cenabat; Neque enim civitas in seditione beata esse potest nec in discordia dominorum domus; Vos autem cum perspicuis dubia debeatis illustrare, dubiis perspicua conamini tollere.</p>",
+    subheading: "This is a subheading.",
+    aside: "<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quod quidem iam fit etiam in Academia.</p>",
+  },
+};
---
+++ src/stories/Molecules/Body/readme.md
@@ -0,0 +1,21 @@
+<!-- This is the general documentation layout. Add or remove any sections as needed, but try to stay consistent across components. -->
+# Body
+
+Description of the Body in markdown.
+
+<details>
+  <summary>Inherited CSS Variables:</summary>
+  - `--name`: description...
+</details>
+
+<details>
+  <summary>Twig Variables:</summary>
+  ```
+  variant: "default",
+  ...,
+  sub_component_data: {
+    variant: "default",
+    ...
+  }
+  ```
+</details>
