7b9768d0d7d025e8de64cc58ec9b24558c8c581e
Philip Boden <philip.boden@gmail.com>
2023-05-09 00:49:41 -0700

    [LWR-181] -- Added breadcrumb component

---
+++ src/stories/Molecules/Breadcrumb/Breadcrumb.data.js
@@ -0,0 +1,22 @@
+export default {
+  default: {
+    breadcrumb: [
+      {
+        url: '#',
+        text: 'Home',
+      },
+      {
+        url: '#',
+        text: 'FIrst Level Parent',
+      },
+      {
+        url: '#',
+        text: 'Second Level Parent',
+      },
+      {
+        url: false,
+        text: 'Current page',
+      },
+    ],
+  },
+};
