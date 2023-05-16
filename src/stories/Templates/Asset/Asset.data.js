7fe8a2f5a6a41ed9de0d5a7032693b2f5efe9413
Calvin Tyndall (sleepingmonk) <sleepingmonk@53471.no-reply.drupal.org>
2023-05-16 14:07:59 -0700

    LWR-199: Fix card button layout. No sidebar, just custom flex.

--- src/stories/Templates/Asset/Asset.data.js
+++ src/stories/Templates/Asset/Asset.data.js
@@ -7,16 +7,18 @@ export default {
   default: {
     variant: "default",
     sub_component_layout: "with-sidebar",
-    sidebar_direction: "row-reverse",
-    sidebar_width: "39ch",
+    sidebar_direction: "row",
+    sidebar_width: "39",
     gap: "s4",
     sub_component_data: [
+      List({ ...WithNoMarker.args }),
       AssetBundle({ ...AssetBundleDefault.args }),
-      '<div class="sidebar">' + List({ ...WithNoMarker.args }) + '</div>',
     ],
   },
   with_no_sidebar: {
     variant: "default",
-    sub_component_data: [AssetBundle({ ...AssetBundleDefault.args })],
+    sub_component_data: [
+      AssetBundle({ ...AssetBundleDefault.args })
+    ],
   },
 };
