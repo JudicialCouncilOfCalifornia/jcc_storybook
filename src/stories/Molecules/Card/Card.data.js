db8285cfbf8e37f770bf6c47f51e951a08b63197
Mel Wong <mel.wong@jud.ca.gov>
2023-05-30 12:45:02 -0700

    LWR-188: card media caption shows if content exists, not toggle

--- src/stories/Molecules/Card/Card.data.js
+++ src/stories/Molecules/Card/Card.data.js
@@ -103,10 +103,7 @@ export default {
     aspect: [],
     media:
       '<iframe width="560" height="315" src="https://www.youtube.com/embed/UpnXQalWR-U?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    media_caption: {
-      show: true,
-      text: "This is caption for the media if provided",
-    },
+    media_caption: "This is caption for the media if provided",
     heading: "Heading For A Card",
     text: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu.</p>",
     button_data: [
@@ -129,10 +126,7 @@ export default {
     aspect: [],
     media:
       '<iframe width="560" height="315" src="https://www.youtube.com/embed/UpnXQalWR-U?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
-    media_caption: {
-      show: true,
-      text: "This is caption for the media if provided",
-    },
+    media_caption: "This is caption for the media if provided",
     heading: "Heading For A Card",
     text: "<p>Lorem ipsum dolor sit amet consectetur adipiscing, elit penatibus dignissim placerat ante vulputate, blandit donec enim senectus ornare. Ac non lacus sollicitudin vulputate sociosqu.</p>",
     button_data: [
