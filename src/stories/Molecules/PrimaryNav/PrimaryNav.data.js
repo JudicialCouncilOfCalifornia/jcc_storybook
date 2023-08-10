75e824ca443ec4285778938800daf4f746fb8dc4
Philip Boden <philip.boden@gmail.com>
2023-08-09 22:27:46 -0700

    [DCAS-60] -- Change desktop main menu (primary with dropdown) to be hover controlled, and no longer click controlled.
    Mobile to stay click controlled. It therefore needs overview link ability and so that ability can now be separated. links and mobile_links can be defined.

--- src/stories/Molecules/PrimaryNav/PrimaryNav.data.js
+++ src/stories/Molecules/PrimaryNav/PrimaryNav.data.js
@@ -199,6 +199,196 @@ export default {
         },
       },
     ],
+    mobile_links: [
+      {
+        text: "Simple Link",
+        is_current: true,
+        id: "nav-section-one",
+        url: "#",
+        section_link: {
+          text: "Navigation link",
+          url: "#",
+        },
+      },
+      {
+        text: "Up To Seven",
+        links: [
+          {
+            text: "Navigation link 1 (Overview)",
+            url: "#",
+            is_overview_link: true,
+          },
+          {
+            text: "Navigation link 2",
+            url: "#",
+          },
+          {
+            text: "Navigation link 3",
+            url: "#",
+            is_current: true
+          },
+          {
+            text: "Navigation link 4",
+            url: "#",
+          },
+          {
+            text: "Navigation link 5",
+            url: "#",
+          },
+          {
+            text: "Navigation link 6",
+            url: "#",
+          },
+          {
+            text: "Navigation link 7",
+            url: "#",
+          },
+        ],
+        section_link: {
+          text: "Navigation link",
+          url: "#",
+        },
+      },
+      {
+        text: "More Than Seven",
+        links: [
+          {
+            text: "Navigation link 1 (Overview)",
+            url: "#",
+            is_overview_link: true,
+          },
+          {
+            text: "Navigation link 2",
+            url: "#",
+          },
+          {
+            text: "Navigation link 3",
+            url: "#",
+          },
+          {
+            text: "Navigation link 4",
+            url: "#",
+          },
+          {
+            text: "Navigation link 5",
+            url: "#",
+          },
+          {
+            text: "Navigation link 6",
+            url: "#",
+          },
+          {
+            text: "Navigation link 7",
+            url: "#",
+          },
+          {
+            text: "Navigation link 8",
+            url: "#",
+          },
+          {
+            text: "Navigation link 9",
+            url: "#",
+            links: [
+              {
+                text: "Navigation link 5.1 (Overview)",
+                url: "#",
+                is_overview_link: true,
+              },
+              {
+                text: "Navigation link 5.2",
+                url: "#",
+              },
+            ],
+          },
+        ],
+        section_link: {
+          text: "Navigation link",
+          url: "#",
+        },
+      },
+      {
+        text: "Lots More",
+        links: [
+          {
+            text: "Navigation link 1 (Overview)",
+            url: "#",
+            is_overview_link: true,
+          },
+          {
+            text: "Navigation link 2",
+            url: "#",
+          },
+          {
+            text: "Navigation link 3",
+            url: "#",
+          },
+          {
+            text: "Navigation link 4",
+            url: "#",
+          },
+          {
+            text: "Navigation link 5",
+            url: "#",
+          },
+          {
+            text: "Navigation link 6",
+            url: "#",
+          },
+          {
+            text: "Navigation link 7",
+            url: "#",
+          },
+          {
+            text: "Navigation link 8",
+            url: "#",
+          },
+          {
+            text: "Navigation link 9",
+            url: "#",
+          },
+          {
+            text: "Navigation link 10",
+            url: "#",
+          },
+          {
+            text: "Navigation link 11",
+            url: "#",
+          },
+          {
+            text: "Navigation link 12",
+            url: "#",
+          },
+          {
+            text: "Navigation link 13",
+            url: "#",
+          },
+          {
+            text: "Navigation link 14",
+            url: "#",
+          },
+          {
+            text: "Navigation link 15",
+            url: "#",
+          },
+          {
+            text: "Navigation link 16",
+            url: "#",
+          },
+          {
+            text: "Navigation link 17",
+            url: "#",
+          },
+          {
+            text: "Navigation link 18",
+            url: "#",
+          },
+        ],
+        section_link: {
+          text: "Navigation link",
+          url: "#",
+        },
+      },
+    ],
   },
   alternate: {
     variant: "alternate",
@@ -211,6 +401,196 @@ export default {
       form: "",
     },
     links: [
+      {
+        text: "Simple Link",
+        is_current: true,
+        id: "nav-section-one",
+        url: "#",
+        section_link: {
+          text: "Navigation link",
+          url: "#",
+        },
+      },
+      {
+        text: "Up To Seven",
+        links: [
+          {
+            text: "Navigation link 1 (Overview)",
+            url: "#",
+            is_overview_link: true,
+          },
+          {
+            text: "Navigation link 2",
+            url: "#",
+          },
+          {
+            text: "Navigation link 3",
+            url: "#",
+            is_current: true
+          },
+          {
+            text: "Navigation link 4",
+            url: "#",
+          },
+          {
+            text: "Navigation link 5",
+            url: "#",
+          },
+          {
+            text: "Navigation link 6",
+            url: "#",
+          },
+          {
+            text: "Navigation link 7",
+            url: "#",
+          },
+        ],
+        section_link: {
+          text: "Navigation link",
+          url: "#",
+        },
+      },
+      {
+        text: "More Than Seven",
+        links: [
+          {
+            text: "Navigation link 1 (Overview)",
+            url: "#",
+            is_overview_link: true,
+          },
+          {
+            text: "Navigation link 2",
+            url: "#",
+          },
+          {
+            text: "Navigation link 3",
+            url: "#",
+          },
+          {
+            text: "Navigation link 4",
+            url: "#",
+          },
+          {
+            text: "Navigation link 5",
+            url: "#",
+          },
+          {
+            text: "Navigation link 6",
+            url: "#",
+          },
+          {
+            text: "Navigation link 7",
+            url: "#",
+          },
+          {
+            text: "Navigation link 8",
+            url: "#",
+          },
+          {
+            text: "Navigation link 9",
+            url: "#",
+            links: [
+              {
+                text: "Navigation link 5.1 (Overview)",
+                url: "#",
+                is_overview_link: true,
+              },
+              {
+                text: "Navigation link 5.2",
+                url: "#",
+              },
+            ],
+          },
+        ],
+        section_link: {
+          text: "Navigation link",
+          url: "#",
+        },
+      },
+      {
+        text: "Lots More",
+        links: [
+          {
+            text: "Navigation link 1 (Overview)",
+            url: "#",
+            is_overview_link: true,
+          },
+          {
+            text: "Navigation link 2",
+            url: "#",
+          },
+          {
+            text: "Navigation link 3",
+            url: "#",
+          },
+          {
+            text: "Navigation link 4",
+            url: "#",
+          },
+          {
+            text: "Navigation link 5",
+            url: "#",
+          },
+          {
+            text: "Navigation link 6",
+            url: "#",
+          },
+          {
+            text: "Navigation link 7",
+            url: "#",
+          },
+          {
+            text: "Navigation link 8",
+            url: "#",
+          },
+          {
+            text: "Navigation link 9",
+            url: "#",
+          },
+          {
+            text: "Navigation link 10",
+            url: "#",
+          },
+          {
+            text: "Navigation link 11",
+            url: "#",
+          },
+          {
+            text: "Navigation link 12",
+            url: "#",
+          },
+          {
+            text: "Navigation link 13",
+            url: "#",
+          },
+          {
+            text: "Navigation link 14",
+            url: "#",
+          },
+          {
+            text: "Navigation link 15",
+            url: "#",
+          },
+          {
+            text: "Navigation link 16",
+            url: "#",
+          },
+          {
+            text: "Navigation link 17",
+            url: "#",
+          },
+          {
+            text: "Navigation link 18",
+            url: "#",
+          },
+        ],
+        section_link: {
+          text: "Navigation link",
+          url: "#",
+        },
+      },
+    ],
+    mobile_links: [
       {
         text: "Simple Link",
         is_current: true,
