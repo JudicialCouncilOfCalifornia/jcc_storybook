4b83c372c8fa55b5fe048c82735193d70acd38a8
Mel Wong <mel.wong@jud.ca.gov>
2023-08-30 17:51:54 -0700

    LWR-165: new read more component

---
+++ src/stories/Molecules/ReadMore/ReadMore.stories.js
@@ -0,0 +1,34 @@
+import readme from './readme.md';
+import data from './ReadMore.data';
+import ReadMore from './ReadMore.twig';
+import './ReadMore.css';
+import './ReadMore.js';
+
+export default {
+  title: 'Molecules/ReadMore',
+  component: ReadMore,
+  parameters: {
+    notes: readme,
+    layout: 'padded',
+  },
+  argTypes: {
+    // Variant select is a pretty common arg.
+    variant: {
+      description: "ReadMore variant.",
+      table: {defaultValue: {summary: 'default'}},
+      control: { type: 'radio' },
+      options: ['default'],
+    },
+    // More args here.
+  },
+};
+
+// Create Template for variant templates to bind to.
+const Template = ({ ...args }) => {
+  return ReadMore({ ...args });
+};
+
+// Bind the Default component variant for this component.
+export const Default = Template.bind({});
+// Data comes from the ReadMore.data.js file.
+Default.args = data.default;
---
+++ src/stories/Molecules/ReadMore/readme.md
@@ -0,0 +1,13 @@
+<!-- This is the general documentation layout. Add or remove any sections as needed, but try to stay consistent across components. -->
+# ReadMore
+
+Component to add initially collapsed content that is revealed by users as needed. Can support more than one instance on a page.
+
+<details>
+  <summary>Twig Variables:</summary>
+
+  ```
+  heading: "Text string",
+  content: "Rendered content",
+  ```
+</details>
