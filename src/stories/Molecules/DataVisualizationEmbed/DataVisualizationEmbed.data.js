d5180a2085eb3aff32b07829105f1e40b54ba0fc
baptisten <baptiste.niviere@gmail.com>
2024-09-25 11:56:42 -0700

    embedded iframes

--- src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
+++ src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
@@ -5,5 +5,6 @@ export default {
     mobile_width: '300',
     mobile_height: '400',
     url: 'https://app.powerbigov.us/view?r=eyJrIjoiYThiYzMyNGMtYTlhNS00ODY0LWE5ZGItZGJmMzJjNjMyNzYxIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9',
-},
+    mobile_url: 'https://app.powerbigov.us/view?r=eyJrIjoiYThiYzMyNGMtYTlhNS00ODY0LWE5ZGItZGJmMzJjNjMyNzYxIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9'
+  },
 };
