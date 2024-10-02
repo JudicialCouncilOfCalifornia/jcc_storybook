280674aedb8f36603630a8a726460f2c318d64ad
baptisten <baptiste.niviere@gmail.com>
2024-10-02 11:15:00 -0700

    css updaet

--- src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
+++ src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
@@ -1,7 +1,7 @@
 export default {
   default: {
-    width: '600',
-    height: '400',
+    width: '1000',
+    height: '600',
     mobile_width: '300',
     mobile_height: '400',
     url: 'https://app.powerbigov.us/view?r=eyJrIjoiYThiYzMyNGMtYTlhNS00ODY0LWE5ZGItZGJmMzJjNjMyNzYxIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9',
