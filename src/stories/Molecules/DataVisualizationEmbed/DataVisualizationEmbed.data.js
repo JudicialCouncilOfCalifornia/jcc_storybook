0a6e8646d550776d6c3cc2765b3b262c1ec0b3c8
baptisten <baptiste.niviere@gmail.com>
2024-10-07 10:49:20 -0700

    edited template

--- src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
+++ src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
@@ -3,7 +3,7 @@ export default {
     width: '1000',
     height: '600',
     mobile_width: '300',
-    mobile_height: '400',
+    mobile_height: '600',
     url: 'https://app.powerbigov.us/view?r=eyJrIjoiYThiYzMyNGMtYTlhNS00ODY0LWE5ZGItZGJmMzJjNjMyNzYxIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9',
     mobile_url: 'https://app.powerbigov.us/view?r=eyJrIjoiYThiYzMyNGMtYTlhNS00ODY0LWE5ZGItZGJmMzJjNjMyNzYxIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9'
   },
