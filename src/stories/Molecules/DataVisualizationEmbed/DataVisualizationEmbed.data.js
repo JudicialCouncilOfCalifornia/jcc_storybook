d3e92f1bfbd142188d58d49b64e25b5587343645
baptisten <baptiste.niviere@gmail.com>
2024-10-09 11:21:01 -0700

    css

--- src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
+++ src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
@@ -1,10 +1,10 @@
 export default {
   default: {
-    width: '1000',
-    height: '600',
-    mobile_width: '300',
-    mobile_height: '600',
-    url: 'https://app.powerbigov.us/view?r=eyJrIjoiYThiYzMyNGMtYTlhNS00ODY0LWE5ZGItZGJmMzJjNjMyNzYxIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9',
-    mobile_url: 'https://app.powerbigov.us/view?r=eyJrIjoiYThiYzMyNGMtYTlhNS00ODY0LWE5ZGItZGJmMzJjNjMyNzYxIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9'
+    width: '1280',
+    height: '660',
+    mobile_width: '475',
+    mobile_height: '950',
+    url: 'https://app.powerbigov.us/view?r=eyJrIjoiYjVmYTg0ZWItMGRiOS00YTdhLThmMzYtYTkwYTYwNGZhOWUyIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9&pageName=a3b2fa020e5d00072b07',
+    mobile_url: 'https://app.powerbigov.us/view?r=eyJrIjoiMDc5YTcxYTAtYTU2NS00NDIzLTk1M2UtYjFlZTZkOWQ3ZmE0IiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9&pageName=a3b2fa020e5d00072b07'
   },
 };
