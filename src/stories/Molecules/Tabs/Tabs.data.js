e6389cd6f7955955ee83d7f3038ab327108ba8b6
Philip Boden <philip.boden@gmail.com>
2024-10-10 23:18:51 -0700

    [COURTS-229] -- Feature to allow for preset active tab in Tab component.
    This is a data attribute set item that is set from a 'tab' query - (/path/of/page?tab=tab-some-id-of-a-tab). Future work will be done to make it a hash of the tab, or hash of an item in a tab.

--- src/stories/Molecules/Tabs/Tabs.data.js
+++ src/stories/Molecules/Tabs/Tabs.data.js
@@ -1,26 +1,29 @@
 export default {
   default: {
     variant: "default",
+    open_on_load: '',
     items: [
       {
         label: "Working With Interpreters",
-        content: "Any rendered content goes here. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus",      },
+        content: "Any rendered content goes here. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus.",
+      },
       {
         label: "Information about Court Processess",
         content: "Any rendered content goes here. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus.",
       },
       {
         label: "Technology and Language Access",
-        content: "Any rendered content goes here. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus",
+        content: "Any rendered content goes here. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus.",
       },
       {
         label: "Additional Resources",
-        content: "Any rendered content goes here. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus",
+        content: "Any rendered content goes here. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus. Lorem ipsum dolor sit amet consectetur adipiscing elit aenean tincidunt erat fames, tempor laoreet a facilisis nam aliquet nisi tellus.",
       },
-],
+    ],
   },
   alternate: {
     variant: "alternate",
+    open_on_load: '',
     items: [
       {
         label: "Featured Items",
