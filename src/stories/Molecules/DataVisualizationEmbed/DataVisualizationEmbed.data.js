9b4b6c021267d441a32d6a9af1836e96d6a9a490
baptisten <baptiste.niviere@gmail.com>
2025-01-21 14:51:32 -0800

    templateupdate

--- src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
+++ src/stories/Molecules/DataVisualizationEmbed/DataVisualizationEmbed.data.js
@@ -5,6 +5,8 @@ export default {
     mobile_width: '475',
     mobile_height: '950',
     url: 'https://app.powerbigov.us/view?r=eyJrIjoiYjVmYTg0ZWItMGRiOS00YTdhLThmMzYtYTkwYTYwNGZhOWUyIiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9&pageName=a3b2fa020e5d00072b07',
-    mobile_url: 'https://app.powerbigov.us/view?r=eyJrIjoiMDc5YTcxYTAtYTU2NS00NDIzLTk1M2UtYjFlZTZkOWQ3ZmE0IiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9&pageName=a3b2fa020e5d00072b07'
+    mobile_url: 'https://app.powerbigov.us/view?r=eyJrIjoiMDc5YTcxYTAtYTU2NS00NDIzLTk1M2UtYjFlZTZkOWQ3ZmE0IiwidCI6IjEwY2ZhMDhhLTViMTctNGU4Zi1hMjQ1LTEzOTA2MmU4MzlkYyJ9&pageName=a3b2fa020e5d00072b07',
+    img_data: '',
+    mobile_img_data: ''
   },
 };
