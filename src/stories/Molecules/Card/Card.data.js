5a61196ffeedf7b13fa13b4ffe67e60ce0873af8
Archana <archana.ramalingam@jud.ca.gov>
2025-07-22 14:31:05 -0700

    White background to card

--- src/stories/Molecules/Card/Card.data.js
+++ src/stories/Molecules/Card/Card.data.js
@@ -104,7 +104,7 @@ export default {
   media_left: {
     variant: "media-left",
     clickable_card: false,
-    background: null,
+    background: 'white',
     aspect: [],
     media:
       '<iframe width="560" height="315" src="https://www.youtube.com/embed/UpnXQalWR-U?controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>',
